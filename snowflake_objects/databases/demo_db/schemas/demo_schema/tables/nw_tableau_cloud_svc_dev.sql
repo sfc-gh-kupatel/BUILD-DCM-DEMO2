4af043ee8b3001768067294aa8aa279ce02dd93c
Kushal Patel <kushal.patel@snowflake.com>
2024-10-22 13:28:24 -0400

    Update nw_tableau_cloud_svc_dev.sql

--- snowflake_objects/databases/demo_db/schemas/demo_schema/tables/nw_tableau_cloud_svc_dev.sql
+++ snowflake_objects/databases/demo_db/schemas/demo_schema/tables/nw_tableau_cloud_svc_dev.sql
@@ -20,7 +20,7 @@ USE DATABASE FHLB_UTILITY;
 USE SCHEMA FHLBUTL;
 
 SET ENV_TABLE_NAME = 'FHLBP_' || $FHLB_ENV || '_TABLE';
-SELECT COUNT(*) FROM IDENTIFIER($ENV_TABLE_NAME);
+--SELECT COUNT(*) FROM IDENTIFIER($ENV_TABLE_NAME);
 
 
 -----------------------------------------------------
@@ -43,4 +43,53 @@ CREATE NETWORK RULE IF NOT EXISTS NETRULE_TABLEAU_CLOUD_EXT
 	MODE= INGRESS
 	COMMENT ='Tableau Cloud IP';
 
+
+-----------------------------------------------------
+-----         Network polices creation          -----
+-----                                           -----
+-----   This section should only be modified    -----
+-----     if a NEW network policy is needed     -----
+-----------------------------------------------------
+USE ROLE SF_NETWORKADMIN;
+USE DATABASE FHLB_UTILITY;
+USE SCHEMA NETWORK;
+
+CREATE NETWORK POLICY IF NOT EXISTS NETPOL_TABLEAU_CLOUD;
+
+
+-----------------------------------------------------
+-----         Network rule assignments          -----
+-----                                           -----
+-----  IP list for each network rule goes here  -----
+-----------------------------------------------------
+USE ROLE SF_NETWORKADMIN;
+USE DATABASE FHLB_UTILITY;
+USE SCHEMA NETWORK;
+
+ALTER NETWORK RULE NETRULE_TABLEAU_CLOUD_FHLB_INT SET
+	VALUE_LIST = ('10.130.3.0/24');
+
+ALTER NETWORK RULE NETRULE_TABLEAU_CLOUD_EXT SET
+	VALUE_LIST = ('3.219.176.16/28','155.226.144.0/22'); --provided by the vendor
+
+
+-----------------------------------------------------
+-----      Network Policy rule assignments      -----
+-----                                           -----
+-----  Rules for each network policy goes here  -----
+-----------------------------------------------------
+USE ROLE SF_NETWORKADMIN;
+USE DATABASE FHLB_UTILITY;
+USE SCHEMA NETWORK;
+
+--ALTER NETWORK POLICY NETPOL_TABLEAU_CLOUD SET
+--	ALLOWED_NETWORK_RULE_LIST = 
+--		( 
+--			'NETRULE_FHLB_DC_EXT', --remove after move to Azure Palo AVD
+--			'NETRULE_FHLB_AZURE_EXT',
+--			'NETRULE_AWS_PL_FHLB_INT',
+--			'NETRULE_TABLEAU_CLOUD_FHLB_INT',
+--			'NETRULE_TABLEAU_CLOUD_EXT'
+--		);
+	--BLOCKED_NETWORK_RULE_LIST = ( 'optional blocked list of IPs');
 
