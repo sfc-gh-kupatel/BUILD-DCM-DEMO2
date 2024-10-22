4c61a23119486802a0a43ecbb416dfadae149a20
Kushal Patel <kushal.patel@snowflake.com>
2024-10-22 14:41:20 -0400

    Update nw_tableau_cloud_svc_dev.sql

--- snowflake_objects/databases/demo_db/schemas/demo_schema/tables/nw_tableau_cloud_svc_dev.sql
+++ snowflake_objects/databases/demo_db/schemas/demo_schema/tables/nw_tableau_cloud_svc_dev.sql
@@ -1,3 +1,27 @@
+-----------------------------------------------------------------------------------
+----- Network rule and policies for tableau cloud
+----- Policies get applied during service account/user creation, not in this script
+-----
+----- Prereqs :
+-----   1.  Standard FHLBP admin roles created
+-----   2.  FHLB_UTILITY creation script run
+-----   3.  Setthe FHLB_ENV variable to the environment you are running this in
+-----------------------------------------------------------------------------------
+
+SET FHLB_ENV = 'DEV';
+
+-----------------------------------------------------
+-----             Verify correct env            -----
+-----------------------------------------------------
+
+-- Determine environment and set username
+-- Replace FHLB_ENV with a public function to determine account/env at a later time
+USE DATABASE FHLB_UTILITY;
+USE SCHEMA FHLBUTL;
+
+SET ENV_TABLE_NAME = 'FHLBP_' || $FHLB_ENV || '_TABLE';
+SELECT COUNT(*) FROM IDENTIFIER($ENV_TABLE_NAME);
+
 
 -----------------------------------------------------
 -----           Network rules creation          -----
